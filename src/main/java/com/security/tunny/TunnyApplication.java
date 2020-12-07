3b14a8b0fbf04bda4f19e746c30f0e26a4ed645e
lxz <liuxianzhie@qq.com>
2020-12-07 10:33:00 +0800

    Initial commit

---
+++ src/main/java/com/security/tunny/TunnyApplication.java
@@ -0,0 +1,13 @@
+package com.security.tunny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TunnyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TunnyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/security/tunny/TunnyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.tunny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TunnyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
